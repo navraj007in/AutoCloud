1a28eb4adce72c52df2a9536185ad9e2dfcda127
Navraj Singh <navraj@outlook.com>
2020-08-15 22:51:55 +1000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoCloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
